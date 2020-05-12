dff5d8e0877f31323155e23f9dcf2928e8c209fb
Cubiwan <cubiwan@gmail.com>
2020-05-13 01:17:45 +0200

    Update simple.ino

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,7 +1,6 @@
 #include <Statistics.h>
 
 Statistics st = Statistics();
-double values[3];
 
 void setup() {
     Serial.begin(9600);     
@@ -32,6 +31,35 @@ void loop() {
     Serial.print("Max: ");
     Serial.println(st.maximum());
 
+    Serial.print("Center: ");
+    Serial.println(st.center());
+
+    Serial.print("Sum: ");
+    Serial.println(st.sum());
+
+    Serial.print("Root Mean Square: ");
+    Serial.println(st.rootMeanSquare());
+
+    Serial.print("Coefficient Of Variation: ");
+    Serial.println(st.coefficientOfVariation());
+    
+    Serial.print("Index Of Dispersion: ");
+    Serial.println(st.indexOfDispersion());
+
+    Serial.print("Zero Crossing: ");
+    Serial.println(st.zeroCrossing());
+
+    Serial.print("Zero Crossing Rate: ");
+    Serial.println(st.zeroCrossingRate());
+
+    Serial.print("Min Max Normalization (5): ");
+    Serial.println(st.minMaxNormalization(5));
+
+    Serial.print("Mean Normalization (5): ");
+    Serial.println(st.meanNormalization(5));
+
+    Serial.print("Standardization (5): ");
+    Serial.println(st.standardization(5));
 
     Serial.println("Reset ");
     st.reset();
